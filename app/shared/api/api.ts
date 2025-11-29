e5ac215732208716e2750e8b85343aefe6bb6fbf
jayli <jayli@example.com>
2025-11-29 13:46:32 +1100

    axios added v4

--- app/shared/api/api.ts
+++ app/shared/api/api.ts
@@ -4,3 +4,19 @@ export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL,
     withCredentials: false,
 });
+
+// axios error handling
+api.interceptors.response.use(
+    (response) => response,    // just return success normally
+    (error) => {
+        const status = error.response?.status;
+
+        // Attach "friendlyMessage" for UI
+        error.friendlyMessage =
+            status === 404 ? "Todo item not found" :
+            status === 401 ? "Unauthorized. Please log in again." :
+                "Something went wrong";
+
+        return Promise.reject(error);
+    }
+);
