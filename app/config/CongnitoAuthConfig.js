2678609a719fd21860a0e4f8b6a299b83d871074
jayli <jayli@example.com>
2026-01-13 20:46:35 +1100

    backup prior to the impl of auto signin

--- app/config/CongnitoAuthConfig.js
+++ app/config/CongnitoAuthConfig.js
@@ -1,3 +1,5 @@
+import {WebStorageStateStore} from "oidc-client-ts";
+
 export const cognitoAuthConfig = {
     authority: `https://cognito-idp.${process.env.NEXT_PUBLIC_AWS_COGNITO_REGION}.amazonaws.com/${process.env.NEXT_PUBLIC_AWS_COGNITO_POOL_ID}`,
     client_id: process.env.NEXT_PUBLIC_AWS_COGNITO_APP_CLIENT_ID,
@@ -5,7 +7,7 @@ export const cognitoAuthConfig = {
     response_type: "code",
     scope: "openid email profile phone",
     automaticSilentRenew: true,
-    // URL cleanup
+    // URL cleanup -- not needed as callback/page.tsx would handle that
     // onSigninCallback: () => {
     //     window.history.replaceState({}, document.title, window.location.pathname);
     // },
